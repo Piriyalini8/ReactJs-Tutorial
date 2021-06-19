51319d64af35d71b132430463eb8d84531c92046
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2021-06-19 17:20:32 +0530

    After V6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React,{Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
+//default export
+// import MyApp from './components/Greet';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+//name export- this is the must to export with same name 
+// import {Greet} from './components/Greet';
 
 class App extends Component {
   render(){
   return (
     <div className="App">
       <Greet/>
+      {/* <MyApp/> */}
+      <Welcome/>
     </div>
   );
   }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function Greet(){
-    return(
-        <h1>Hello Priya</h1>
-    );
-}
+// function Greet(){
+//     return(
+//         <h1>Hello Priya</h1>
+//     );
+// }
+
+const Greet=()=><h1>Hello Priya</h1>
+
+// //name export
+// export const Greet=()=><h1>Hello Priya</h1>
+
 export default Greet;
